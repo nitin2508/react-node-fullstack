fca1558d45a642905aae201729fdaca0bbddfd06
Nitin Nagar <25nitin.nagar@gmail.com>
2017-09-16 01:52:01 +0530

    Update dev.js

--- config/dev.js
+++ config/dev.js
@@ -1,10 +1,10 @@
 module.exports = {
-  googleClientId:'966870994082-d1iroeje286p7i73m811e54ldg2ru4rd.apps.googleusercontent.com',
-  googleClientSecret:'UO3rKRsFQATO2uCwpyQrVZTK',
-  mongoURI:'mongodb://nitinnagar:password@ds153003.mlab.com:53003/emaily-dev',
+  googleClientId:'966870994082-d1iroeje286p7i73m811e54ldg2ru4.apps.googleusercontent.com',
+  googleClientSecret:'UO3rKRsFQATO2uCwpyQrVZ',
+  mongoURI:'mongodb://nitinnagar:password123@ds153003.mlab.com:53003/emaily-dev',
   cookieKey:'1234567890',
-  stripePublishableKey:'pk_test_9o6Nwnr1XQflQ0fLOtdcqVU7',
-  stripeSecretKey:'sk_test_Htde6M2EfB77u3vx73kduIBG',
-  sendGridKey:'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',
+  stripePublishableKey:'pk_test_9o6Nwnr1XQflQ0fLOtdcqV',
+  stripeSecretKey:'sk_test_Htde6M2EfB77u3vx73kduI',
+  sendGridKey:'SG.NW5eq6ISSYy2h7-HXEPNOg.a0UKN_bWgWbxSmrRwRLoNjlCGPiucR7PDQ54P--KQ',
   redirectDomain:'http://localhost:3000'
 };
